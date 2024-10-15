b9e2f977ad67cae4e4c2ba552f975200be3eebbc
NicolasRivasR <nicodesar@gmail.com>
2024-10-15 17:51:45 +0200

    Cross origin en RegistrationControler

--- Back-End/redSocial/src/main/java/com/nicolas/redSocial/controller/RegistrationControler.java
+++ Back-End/redSocial/src/main/java/com/nicolas/redSocial/controller/RegistrationControler.java
@@ -4,11 +4,13 @@ import com.nicolas.redSocial.models.User;
 import com.nicolas.redSocial.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class RegistrationControler {
     
     @Autowired
@@ -33,8 +35,12 @@ public class RegistrationControler {
     @PostMapping("/register/user")
     public User createNewUser(@RequestBody User user){
         
+        System.out.println("Voy a regitrar un nuevo usuario");
+        
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return userRepository.save(user);
+        return user;
+        //return userRepository.save(user);
+        //
         
     }
 
