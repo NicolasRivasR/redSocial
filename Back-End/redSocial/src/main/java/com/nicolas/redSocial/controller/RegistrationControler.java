b6f1ff1b05f5cfd16653f74f04b7911ac36821a4
NicolasRivasR <nicodesar@gmail.com>
2024-10-17 01:04:07 +0200

    Encontrado fallo, al llamar a manager

--- Back-End/redSocial/src/main/java/com/nicolas/redSocial/controller/RegistrationControler.java
+++ Back-End/redSocial/src/main/java/com/nicolas/redSocial/controller/RegistrationControler.java
@@ -2,7 +2,13 @@ package com.nicolas.redSocial.controller;
 
 import com.nicolas.redSocial.models.User;
 import com.nicolas.redSocial.repository.UserRepository;
+import com.nicolas.redSocial.service.UserService;
+import com.nicolas.redSocial.webtoken.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,14 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin("*")
 public class RegistrationControler {
-    
+
     @Autowired
     private UserRepository userRepository;
-    
+
     @Autowired
     private PasswordEncoder passwordEncoder;
-            
-    
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
     /*
     {
         "username": "john_doe1",
@@ -31,17 +45,28 @@ public class RegistrationControler {
         "profilePicture": "https://example.com/profile/johndoe.jpg",
         "role": "USER"
     }
-    */
+     */
     @PostMapping("/register/user")
-    public User createNewUser(@RequestBody User user){
-        
+    public String createNewUser(@RequestBody User user) {
+
         System.out.println("Voy a regitrar un nuevo usuario");
-        
+
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return user;
+
+        userRepository.save(user);
+
+        System.out.println("Vamo a autenticalno");
+        Authentication res = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())); //ESTO ES LO QUE HACE QUE FALLE  
+
+        if (res.isAuthenticated()) {
+            System.out.println("Autenticado");
+            return jwtService.generateToken(userService.loadUserByUsername(user.getUsername()));
+        } else {
+            throw new UsernameNotFoundException("Invalid Credentials");
+        }
         //return userRepository.save(user);
         //
-        
+
     }
 
 }
